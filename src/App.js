ff66e452e480a6561bb111ba37c7a1751a9edd1c
고나은 <gonaeunn@gmail.com>
2024-12-17 21:22:54 +0900

    백엔드와 소켓통신 연결 완료
    터미널에 소켓id 뜸

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import socket from "./server";
+// import socket from "./server";
+import { io } from "socket.io-client";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <div className="App"></div>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const socket = io("http://localhost:5002"); // 백엔드 주소
+
+  useEffect(() => {
+    // 연결 성공 시 상태 업데이트
+    socket.on("connect", () => {
+      console.log("Connected to server with socket ID:", socket.id);
+      setIsConnected(true);
+    });
+
+    // 연결 해제 시 상태 업데이트
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+      setIsConnected(false);
+    });
+
+    // Cleanup
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <div>
-      <div className="App"></div>
+    <div className="container">
+      {isConnected ? (
+        <div className="connected">서버에 연결되었습니다!</div>
+      ) : (
+        <div className="disconnected">서버 연결을 확인하세요.</div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import {io} from "socket.io-client"
-const socket = io("http://localhost:5004")   // 연결할 백엔드 주소
+const socket = io("http://localhost:5002")   // 연결할 백엔드 주소
 export default socket;
