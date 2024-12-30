8e062f58f37cf2060701e630997f8021f38a9112
고나은 <gonaeunn@gmail.com>
2024-12-30 21:30:12 +0900

    채팅방 목록에서 참여자 숫자와. 채팅방 내의 join 메세지 확인

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"; 
 import MessageContainer from "../../components/MessageContainer/MessageContainer";
 import InputField from "../../components/InputField/InputField";
 import "./chatPageStyle.css";
 
 const ChatPage = ({ user, socket }) => {
+  const { id } = useParams(); // URL에서 방 ID 가져오기
   const [messageList, setMessageList] = useState([]);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     if (!socket) return;
 
+    console.log("Joining room with ID:", id); // 디버깅 로그
+
+    // 방에 참여 요청
+    socket.emit("joinRoom", id, (res) =>{
+      console.log("Server response:", res); // 서버 응답 로그
+      if (res && res.ok) {
+        console.log("Successfully joined the room", res);        
+      } else {
+        console.error("Failed to join the room", res);
+      }
+    });
+
+    // 메세지 수신 핸들러
     socket.on("message", (res) => {
       console.log("Received message:", res);
       setMessageList((prevState) => [...prevState, res]);
@@ -18,7 +33,8 @@ const ChatPage = ({ user, socket }) => {
     return () => {
       socket.off("message"); // 이벤트 리스너 제거
     };
-  }, [socket]);
+  // }, [socket]);
+  }, [socket, id]);  // id를 의존성 배열에 추가 하라고 gpt가 그러는데 일단 주석처리
 
   const sendMessage = (event) => {
     event.preventDefault();
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-// import {io} from "socket.io-client"
-// const socket = io("http://localhost:5002")   // 연결할 백엔드 주소
-// export default socket;
+import {io} from "socket.io-client"
+const socket = io("http://localhost:5002")   // 연결할 백엔드 주소
+export default socket;
